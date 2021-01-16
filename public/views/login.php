85f358f9e0db7721c7d2d6f46352ed1bd4031198
pmasior <pmasior@outlook.com>
2021-01-16 22:03:23 +0100

    Modify CSS, add register button on login page

--- public/views/login.php
+++ public/views/login.php
@@ -3,26 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="/public/css/style.css">  <!-- TODO: w labach bez /-->
+    <link rel="stylesheet" type="text/css" href="/public/css/login-style.css">
     <script type="text/javascript" src="/public/js/script.js" defer></script>
     <title>Communitly - login</title>
 </head>
 <body>
-    <div class="container">
-    <!-- TODO: przycisk do rejestracji -->
-        <div class="logo logo-container">Communitly</div>
-        <div class="login-container">
-            <form action="signIn" method="post">
-                <input type="text" name='email' placeholder="Login" value="example@example.com" autofocus>  <!-- TODO: delete value -->
-                <input type="password" name="pass" placeholder="Password">  <!-- TODO: change pass -> password -->
-                <?php if(isset($messages)): ?>
-                    <?php foreach ($messages as $message): ?>
-                        <span class="error"><?= $message; ?></span>
-                    <?php endforeach; ?>
-                <?php endif; ?>
-                <input type="submit" value="Zaloguj się">
-            </form>
-        </div>
+    <header>
+        <a href="/register" class="button header-login-button">Zarejestruj się</a>
+    </header>
+    <div class="logo-container">
+        <a href="/" class="logo">Communitly</a>
+    </div>
+    <div class="login-container">
+        <form action="signIn" method="post">
+            <input class="input-with-text" type="text" name='email' placeholder="Login" value="example@example.com" autofocus>  <!-- TODO: delete value -->
+            <input class="input-with-text" type="password" name="pass" placeholder="Password">  <!-- TODO: change pass -> password -->
+            <?php if (isset($messages)): ?>
+                <?php foreach ($messages as $message): ?>
+                    <span class="error"><?= $message; ?></span>
+                <?php endforeach; ?>
+            <?php endif; ?>
+            <input class="button button-in-form" type="submit" value="Zaloguj się">
+        </form>
     </div>
 </body>
 </html>
--- public/views/register.php
+++ public/views/register.php
@@ -3,29 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="/public/css/style.css">  <!-- TODO: w labach bez /-->
+    <link rel="stylesheet" type="text/css" href="/public/css/login-style.css">
     <script type="text/javascript" src="/public/js/script.js" defer></script>
     <title>Communitly - login</title>
 </head>
 <body>
-    <div class="container">
-    <!-- TODO: przycisk do rejestracji -->
-        <div class="logo logo-container">Communitly</div>
-        <div class="login-container">
-            <form action="signUp" method="post">
-                <input type="text" name='email' placeholder="Login" value="example@example.com" autofocus>  <!-- TODO: delete value -->
-                <input type="password" name="password" placeholder="Password">  <!-- TODO: change pass -> password -->
-                <input type="password" name="confirmedPassword" placeholder="Confirm password">  <!-- TODO: change pass -> password -->
-                <input type="text" name="firstName" placeholder="First name">  <!-- TODO: change pass -> password -->
-                <input type="text" name="lastName" placeholder="Last name">  <!-- TODO: change pass -> password -->
-                <?php if(isset($messages)): ?>
-                    <?php foreach ($messages as $message): ?>
-                        <span class="error"><?= $message; ?></span>
-                    <?php endforeach; ?>
-                <?php endif; ?>
-                <input type="submit" value="Zarejestruj się">
-            </form>
-        </div>
+    <header>
+        <a href="/" class="button header-login-button">Zaloguj się</a>
+    </header>
+    <div class="logo-container">
+        <a href="/" class="logo">Communitly</a>
+    </div>
+    <div class="login-container">
+        <form action="signUp" method="post">
+            <input class="input-with-text" type="text" name='email' placeholder="Login" value="example@example.com" autofocus>  <!-- TODO: delete value -->
+            <input class="input-with-text" type="password" name="password" placeholder="Password">
+            <input class="input-with-text" type="password" name="confirmedPassword" placeholder="Confirm password">
+            <input class="input-with-text" type="text" name="firstName" placeholder="First name">
+            <input class="input-with-text" type="text" name="lastName" placeholder="Last name">
+            <?php if (isset($messages)): ?>
+                <?php foreach ($messages as $message): ?>
+                    <span class="error"><?= $message; ?></span>
+                <?php endforeach; ?>
+            <?php endif; ?>
+            <input class="button button-in-form" type="submit" value="Zarejestruj się">
+        </form>
     </div>
 </body>
 </html>
